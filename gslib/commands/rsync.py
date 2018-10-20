f25475afeec1a98374cf1a5d436d2b6030385305
John Gruska <johngruska@gmail.com>
2018-10-20 11:29:46 -0400

    add print line to help debug issue

--- gslib/commands/rsync.py
+++ gslib/commands/rsync.py
@@ -712,6 +712,7 @@ def _FieldedListingIterator(cls, gsutil_api, base_url_str, desc):
         str_to_check = str_to_check[1:]
       if cls.exclude_pattern.match(str_to_check):
         continue
+    print("Trying: ".url.object_name)  
     # For HAF purposes: ignore broken symbolic links!  :)
     if (os.path.islink(url.object_name) and not os.path.exists(url.object_name)):
       print("Bad link: ".url.object_name)
